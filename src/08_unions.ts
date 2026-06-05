8dce83415abb63269dd8af5d06278d9f6ceb0f6c
Prakash <sdevapujyula@gmail.com>
2026-06-05 10:34:36 +0530

    dded unions in ts

---
+++ src/08_unions.ts
@@ -0,0 +1,39 @@
+// value -> this OR that
+
+function printId(id: string | number) {
+  //   if(typeof id === 'string') {
+  //     id.to
+  //   } else {
+  //     id.toFixed(2)
+  //   }
+}
+
+// Object union
+type Admin = { role: "Admin"; permissions: string[] };
+type Customer = { role: "Customer"; loyaltyPoints: number };
+
+function describeUser(u: Admin | Customer) {
+  if (u.role === "Admin") {
+    console.log(u.permissions);
+  } else {
+    console.log(u.loyaltyPoints);
+  }
+}
+
+// enum operator
+
+function describeUserWithInOperator(u: Admin | Customer) {
+  if ("permissions" in u) {
+    console.log(u.role, "Admin user");
+  } else {
+    console.log(u.loyaltyPoints);
+  }
+}
+
+// array of unions and union of arrays
+const arrayOfUnion: (string | number)[] = ["a", 1, "b", "2"];
+
+const unionOfArrays: string[] | number[] =
+  Math.random() > 0.1 ? ["x", "3"] : [1, 2];
+
+  // unionOfArrays.push(2)
